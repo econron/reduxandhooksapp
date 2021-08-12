f8470165263d14abd1e9639d2cee86b236316cdd
econron <aroneconbusi5544@gmail.com>
2021-08-12 12:03:27 +0900

    useStateを利用した例
    set〇〇　は　() => set〇〇　で描いてあげるとエラーにならない。
    繰り返し呼び出す感じになるのを防ぐ。

--- hooksandreduxapp/src/App.js
+++ hooksandreduxapp/src/App.js
@@ -1,38 +1,36 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = props => {
+  // useStateで初期値を割り当てる
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+  const resetAllParams = () => {
+    setName(props.name);
+    setPrice(props.price);
+  };
 
-  const increment = () => setCount(count + 1);
-  const decrement = () => setCount(count - 1);
-  const increment2 = () => setCount(previousCount => previousCount + 1);
-  const decrement2 = () => setCount(previousCount => previousCount - 1);
-  const reset = () => setCount(0);
-  const divisionFour = () => {
-    if(count % 4 !== 0) return;
-    setCount(count / 4);
-  }
-
-
+  // onClick内で　() => set〇〇()　と書かないと
+  // react-dom.development.js:14997 Uncaught Error: Too many re-renders. 
+  // React limits the number of renders to prevent an infinite loop.
+  // いまいち理由がわからない
   return (
   <>
+    <p>現在の{name}は{price}円です。</p>
     <div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-    </div>
-    <div>
-      <button onClick={increment2}>+1</button>
-      <button onClick={decrement2}>-1</button>
-    </div>
-    <div>
-      <button onClick={reset}>RESET</button>
-      <button onClick={divisionFour}>４の倍数の時だけ４で割る</button>
-    </div>
-    <div>
-      count: {count}
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={resetAllParams}>RESET</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
     </div>
   </>
   );
+};
+
+// 公式DOCによればdefaultPropsで、
+// undefined(nullはなし)のprops.x(xは名付けたフィールド名)に値を割り当てる。
+App.defaultProps = {
+  name: '',
+  price: 1000
 };
 
 export default App;
