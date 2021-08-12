a79ea3bf74c582999195c61ff30a2c489afd839c
econron <aroneconbusi5544@gmail.com>
2021-08-12 12:21:46 +0900

    リファクタリング
    state1つで管理するために
    ・name, priceをstateにまとめた
    ・setName, setPriceをsetStateにまとめた
    
    state.nameは長くなるので、あらかじめ宣言して短くした。

--- hooksandreduxapp/src/App.js
+++ hooksandreduxapp/src/App.js
@@ -1,33 +1,26 @@
 import React, { useState } from 'react';
 
-const App = props => {
-  // useStateで初期値を割り当てる
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-  const resetAllParams = () => {
-    setName(props.name);
-    setPrice(props.price);
-  };
+// 主目的：name, priceではなくstate1つで管理する
 
-  // onClick内で　() => set〇〇()　と書かないと
-  // react-dom.development.js:14997 Uncaught Error: Too many re-renders. 
-  // React limits the number of renders to prevent an infinite loop.
-  // いまいち理由がわからない
+const App = props => {
+  const [state, setState] = useState(props);
+  // オブジェクト方式で渡す　理由：オブジェクトで渡しているから。
+  const { name, price } = state;
+
+  // spread構文を利用することで、そのほかの引数は値をそのままにできる。（公式DOC解説より）
   return (
   <>
     <p>現在の{name}は{price}円です。</p>
     <div>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={resetAllParams}>RESET</button>
-      <input value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={() => setState({...state, price : price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price : price - 1})}>-1</button>
+      <button onClick={() => setState(props)}>RESET</button>
+      <input value={name} onChange={e => setState({...state, name : e.target.value})} />
     </div>
   </>
   );
 };
 
-// 公式DOCによればdefaultPropsで、
-// undefined(nullはなし)のprops.x(xは名付けたフィールド名)に値を割り当てる。
 App.defaultProps = {
   name: '',
   price: 1000
