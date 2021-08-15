c5c55052aa398ee4860a53d8089ec9f709c6a0b4
econron <aroneconbusi5544@gmail.com>
2021-08-15 15:11:50 +0900

    コンポーネント分割
    利用している関数や変数を満たして移植する。
    引数として渡す場合、{}を渡してやると良い。

--- hooksandreduxapp/src/components/App.js
+++ hooksandreduxapp/src/components/App.js
@@ -1,70 +1,16 @@
 import React, { useState, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers';
-import Event from './Event';
+import Events from './Events';
+import EventForm from './EventForm.js';
 
 const App = () => {
+
   const [state, dispatch] = useReducer(reducer, []); 
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const addEvent = e => {
-      // リロードをさせない
-      e.preventDefault();
-
-      // addEvent起動時、reducer/index.jsの処理を実行する上で必要な値をパスする。
-      dispatch({
-          // reducerのactionに渡す引数一覧
-          // typeは必須
-          type: 'CREATE_EVENT',
-          title,
-          body
-      });
-
-      // stateに配列として保存後、表示する値を初期化。
-      setTitle('');
-      setBody('');
-  };
-
-  const deleteAllEvents = e => {
-      e.preventDefault();
-      dispatch({
-          type: 'DELETE_ALL_EVENTS'
-      });
-  }
-    
   return(
       <>
-      <div className="container-fluid">
-          <h4>イベント作成フォーム</h4>
-      </div>
-      <form>
-          <div className="form-group">
-              <label htmlFor="formEventTitle">タイトル</label>
-              <input className="form-control" id="formEventTitle" 
-              value={title} onChange={e => setTitle(e.target.value)} />
-          </div>
-          <div className="form-group">
-              <label htmlFor="formEventBody">ボディー</label>
-              <textarea className="form-control" id="formEventBody" 
-              value={body} onChange={e => setBody(e.target.value)}/>
-          </div>
-          <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-          <button className="btn btn-danger" onClick={deleteAllEvents}>全てのイベントを削除する</button>
-      </form>
-
-      <h4>イベント一覧</h4>
-      <table className="table table-hover">
-          <thead>
-              <tr>
-                  <th>ID</th>
-                  <th>タイトル</th>
-                  <th>ボディー</th>
-              </tr>
-          </thead>
-          <tbody>
-              { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
-          </tbody>
-      </table>
+      <EventForm state={state} dispatch={dispatch}/>
+      <Events state={state} dispatch={dispatch} />
       </>
   );
 };
---
+++ hooksandreduxapp/src/components/EventForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+
+// 引数として渡す場合、{}　の中に値を渡していく。
+const EventForm = ({ state, dispatch }) => {
+
+    // useReducerを呼び出した時点で初期化されている。
+    // よって、app.jsで呼び出しても値が生成されない。
+    // 同じstateを参照するように改修する。
+    // const [state, dispatch] = useReducer(reducer, []); 
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const addEvent = e => {
+        e.preventDefault();
+        dispatch({
+        // reducerのactionに渡す引数一覧
+        // typeは必須
+        type: 'CREATE_EVENT',
+        title,
+        body
+        });
+
+        // stateに配列として保存後、表示する値を初期化。
+        setTitle('');
+        setBody('');
+    };
+
+
+    const deleteAllEvents = e => {
+        e.preventDefault();
+        dispatch({
+          type: 'DELETE_ALL_EVENTS'
+        });
+    };
+
+    return (
+        <>
+        <div className="container-fluid">
+          <h4>イベント作成フォーム</h4>
+        </div>
+        <form>
+            <div className="form-group">
+              <label htmlFor="formEventTitle">タイトル</label>
+              <input className="form-control" id="formEventTitle" 
+              value={title} onChange={e => setTitle(e.target.value)} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="formEventBody">ボディー</label>
+              <textarea className="form-control" id="formEventBody" 
+              value={body} onChange={e => setBody(e.target.value)}/>
+            </div>
+            <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
+            <button className="btn btn-danger" onClick={deleteAllEvents}>全てのイベントを削除する</button>
+        </form>
+        </>
+    );
+}
+
+export default EventForm;
