5612589938141cc2aaae6826d9a81e662cef7334
econron <aroneconbusi5544@gmail.com>
2021-08-15 16:01:09 +0900

    一部コメント追記。
    リファクタリングをしてきたが、今後ベストプラクティスが出てきた場合、下記手順は変更されるかも。
    
    作成順序
    ①要件整理。状態遷移図を利用して、実装漏れをなくす試みをする。
    ②状態遷移図に基づきreducerやstateを利用して状態管理。
    ③作成したソースをコンポーネント分割。
     ④変数の状態の見通しをよくするため、contexで置き換え。

--- hooksandreduxapp/src/components/App.js
+++ hooksandreduxapp/src/components/App.js
@@ -5,6 +5,12 @@ import Events from './Events';
 import EventForm from './EventForm.js';
 import AppContext from '../contexts/AppContext';
 
+//　作成順序
+// ①要件整理。状態遷移図を利用して、実装漏れをなくす試みをする。
+// ②状態遷移図に基づきreducerやstateを利用して状態管理。
+// ③作成したソースをコンポーネント分割。
+// ④変数の状態の見通しをよくするため、contexで置き換え。
+
 const App = () => {
 
   const [state, dispatch] = useReducer(reducer, []); 
